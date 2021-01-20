09bf37978b91069fb59f8ceeb25e3c45711bda0c
xugarxu <2793821834@qq.com>
2021-01-20 18:37:47 +0800

    初始化版本项目

---
+++ process_script/raw_2_pkl/process_text.py
@@ -0,0 +1,45 @@
+import pickle
+import jieba
+import os
+import re
+import string
+
+pos_words = []
+neg_words = []
+
+
+FindPath = '../../raw_data/tagged_data/pos/'
+FileNames = os.listdir(FindPath)
+
+for file_name in FileNames:
+    full_file_name = os.path.join(FindPath, file_name)
+    if 'utf8' in full_file_name:
+        with open(full_file_name, 'r', encoding='utf-8') as pos_f:
+            pos_text = pos_f.read()
+            pos_text = ''.join(pos_text.split())
+            pos_text = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）～-]+", "", pos_text) # 去除标点符号
+            pos_list = jieba.cut(pos_text, cut_all=False)
+            pos_words.append(list(pos_list))
+
+
+FindPath = '../../raw_data/tagged_data/neg/'
+FileNames = os.listdir(FindPath)
+for file_name in FileNames:
+    full_file_name = os.path.join(FindPath, file_name)
+    if 'utf8' in full_file_name:
+        with open(full_file_name, 'r', encoding='utf-8') as neg_f:
+            neg_text = neg_f.read()
+            neg_text = ''.join(neg_text.split())
+            # neg_text = re.sub(string.punctuation, "", neg_text)
+            neg_text = re.sub("[\s+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）～-]+", "", neg_text)
+            neg_list = jieba.cut(neg_text, cut_all=False)
+            neg_words.append(list(neg_list))
+
+
+output = open('../../pkl_data/tagged_data/pos_review.pkl', 'wb')
+pickle.dump(pos_words, output)
+output.close()
+
+output = open('../../pkl_data/tagged_data/neg_review.pkl', 'wb')
+pickle.dump(neg_words, output)
+output.close()
