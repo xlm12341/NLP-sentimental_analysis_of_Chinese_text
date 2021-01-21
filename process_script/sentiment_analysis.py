64484411cc8232d8a04e7f01001fbc72e39ee360
xugarxu <2793821834@qq.com>
2021-01-21 14:44:14 +0800

    plot ROC curves

--- process_script/sentiment_analysis.py
+++ process_script/sentiment_analysis.py
@@ -6,14 +6,20 @@ import itertools
 import nltk
 import os
 import sklearn
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import svm, datasets
+from sklearn.metrics import roc_curve, auc
 from nltk.collocations import BigramCollocationFinder
 from nltk.metrics import BigramAssocMeasures
 from nltk.probability import FreqDist, ConditionalFreqDist
 from nltk.classify.scikitlearn import SklearnClassifier
+from sklearn.model_selection import StratifiedKFold
 from sklearn.svm import SVC, LinearSVC, NuSVC
 from sklearn.naive_bayes import MultinomialNB, BernoulliNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
+
 
 pos_f = '../pkl_data/tagged_data/pos_review.pkl'
 neg_f = '../pkl_data/tagged_data/neg_review.pkl'
@@ -185,7 +191,7 @@ def cut_data(posFeatures, negFeatures):
     # test_data = posFeatures[:500] + negFeatures[:500]
     train = posFeatures[300:] + negFeatures[300:]  # 后800条作训练
     devtest = posFeatures[:300] + negFeatures[:300]  # 前200条作开发测试
-
+    # 这里采用了手动分类，实际上不科学，应该调包KFold，GroupKFold，StratifiedKFold
 
 # 4.1 开发测试集分割人工标注的标签和数据
 def cut_devtest():
@@ -199,37 +205,90 @@ def cut_devtest():
 # i = 0
 def score(classifier):
     classifier = nltk.SklearnClassifier(classifier)  # 在nltk 中使用scikit-learn的接口
-    classifier.train(train)  #训练分类器
+    classifier.train(train)  # 训练分类器
 
     pred = classifier.classify_many(dev)  # 对开发测试集的数据进行分类，给出预测的标签
     a_s = accuracy_score(tag_dev, pred)
-    p_s = precision_score(tag_dev, pred, average='binary',pos_label="pos")
-    r_s = recall_score(tag_dev, pred, average='binary',pos_label="pos")
-    f1_s = f1_score(tag_dev, pred, average='binary',pos_label="pos")
-    return (a_s, p_s, r_s, f1_s)  # 对比分类预测结果和人工标注的正确结果，给出分类器准确度
+    p_s = precision_score(tag_dev, pred, average='binary', pos_label="pos")
+    r_s = recall_score(tag_dev, pred, average='binary', pos_label="pos")
+    f1_s = f1_score(tag_dev, pred, average='binary', pos_label="pos")
+    return a_s, p_s, r_s, f1_s  # 对比分类预测结果和人工标注的正确结果，给出分类器准确度
 
+def plot_ROC(classifier):
+    cv = StratifiedKFold(n_splits=6)  # 导入该模型，后面将数据划分6份
+    classifier = SklearnClassifier(classifier)
+
+    # 画平均ROC曲线的两个参数
+    mean_tpr = 0.0  # 用来记录画平均ROC曲线的信息
+    mean_fpr = np.linspace(0, 1, 100)
+    cnt = 0
+
+    for i, (train, test) in enumerate(cv.split(dev, tag_dev)):  # 利用模型划分数据集和目标变量 为一一对应的下标
+        cnt += 1
+        devtest1 = np.array(devtest)
+        classifier.train(devtest1[train])
+        # print(cnt)
+        # print(train)
+
+        dev1 = np.array(dev)
+        tag_dev1 = np.array(tag_dev)
+        pred_ = classifier.prob_classify_many(dev1[test]) # 测试集的概率
+        # print(pred_)
+        # probas_ = classifier.fit(dev1[train], tag_dev1[train]).predict_proba(dev1[test])  # 训练模型后预测每条样本得到两种结果的概率
+        probas_ = []
+        for j in pred_:
+            probas_.append(j.prob('pos'))
+        fpr, tpr, thresholds = roc_curve(tag_dev1[test], probas_, pos_label='pos')  # 该函数得到伪正例、真正例、阈值，这里只使用前两个
+        # print(fpr)
+        mean_tpr += np.interp(mean_fpr, fpr, tpr)  # 插值函数 interp(x坐标,每次x增加距离,y坐标)  累计每次循环的总值后面求平均值
+        mean_tpr[0] = 0.0  # 将第一个真正例=0 以0为起点
+
+        roc_auc = auc(fpr, tpr)  # 求auc面积
+        plt.plot(fpr, tpr, lw=1, label='ROC fold {0:.2f} (area = {1:.2f})'.format(i, roc_auc))  # 画出当前分割数据的ROC曲线
+
+    plt.plot([0, 1], [0, 1], '--', color=(0.6, 0.6, 0.6), label='Luck')  # 画对角线
+
+    mean_tpr /= cnt  # 求数组的平均值
+    mean_tpr[-1] = 1.0  # 坐标最后一个点为（1,1）  以1为终点
+    mean_auc = auc(mean_fpr, mean_tpr)
+
+    plt.plot(mean_fpr, mean_tpr, 'k--', label='Mean ROC (area = {0:.2f})'.format(mean_auc), lw=2)
+
+    plt.xlim([-0.05, 1.05])  # 设置x、y轴的上下限，设置宽一点，以免和边缘重合，可以更好的观察图像的整体
+    plt.ylim([-0.05, 1.05])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')  # 可以使用中文，但需要导入一些库即字体
+    plt.title('Receiver operating characteristic curve using {0}'.format(classifier))
+    plt.legend(loc="lower right")
+    plt.show()
 
 def try_diffirent_classifiers():
 
     results = list()
     results.append(score(BernoulliNB()))
+    plot_ROC(BernoulliNB())
     results.append(score(MultinomialNB()))
+    plot_ROC(MultinomialNB())
     results.append(score(LogisticRegression(max_iter=10000)))
+    plot_ROC(LogisticRegression(max_iter=10000))
     results.append(score(SVC()))
+    # plot_ROC(SVC)
     results.append(score(LinearSVC(max_iter=10000)))
+    # plot_ROC(LinearSVC(max_iter=10000))
     results.append(score(NuSVC()))
+    # plot_ROC(NuSVC())
 
     return results
 
 
 best_words = []
-
+classifiers = ['BernoulliNB', 'MultinomiaNB', 'LogisticRegression', 'SVC', 'LinearSVC', 'NuSVC']
 
 # 4.5 检验不同分类器和不同的特征选择的结果
 def compare_test():
 
     global pos_review, neg_review
-    classifiers = ['BernoulliNB', 'MultinomiaNB', 'LogisticRegression', 'SVC', 'LinearSVC', 'NuSVC']
+
 
     load_data()
 
@@ -381,8 +440,6 @@ def compare_test():
             temp += 1
         row_cnt += 1
 
-
-
     # 保存文件
     if(os.path.exists('../out/compare.xls')):
         os.remove('../out/compare.xls')
@@ -472,6 +529,7 @@ def application(moto_features):
     clf = pickle.load(open('../out/classifier.pkl', 'rb'))  # 载入分类器
 
     pred = clf.prob_classify_many(moto_features)  # 该方法是计算分类概率值的
+    print(pred)
     p_file = open('../out/test_result.txt', 'w')  # 把结果写入文档
     for i in pred:
         p_file.write(str(i.prob('pos')) + ' ' + str(i.prob('neg')) + '\n')
@@ -479,9 +537,9 @@ def application(moto_features):
 
 
 if __name__ == '__main__':
-    #store_classifier()
-    #moto_features = transfer_text_to_moto()
-    #application(moto_features)
+    # store_classifier()
+    # moto_features = transfer_text_to_moto()
+    # application(moto_features)
     compare_test()
 
 
